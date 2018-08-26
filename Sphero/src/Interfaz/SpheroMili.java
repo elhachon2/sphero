4933c977b2bc7ae1c0934578b5a3a5ad31dfbe56
Milagros <milagros.paoletti@gmail.com>
2018-08-25 22:59:42 -0300

    Mejora de la interfaz.

--- Sphero/src/Interfaz/SpheroMili.java
+++ Sphero/src/Interfaz/SpheroMili.java
@@ -4,6 +4,7 @@ import Simulacion.*;
 import SpheroApp.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Vector;
@@ -35,16 +36,26 @@ public class SpheroMili {
     private JTextField tablero1;
     private JButton ejecutar;
     private JTextArea tablero;
+    private JButton agregar;
+    private JComboBox accionesAAgregar;
+    private JLabel elegirAcciones;
+    private JList list1;
+    private JButton borrarAccion;
+    private JTextArea codigoToSphero;
+    private JButton codigoParaSpheroButton;
     private Vector<AccionApp> vectorDeAcciones;
+    private DefaultListModel modelo;
 
     public void ActualizarComboBox() {
         listAcciones.removeAllItems();
         accionesIf.removeAllItems();
         accionesElse.removeAllItems();
+        accionesAAgregar.removeAllItems();
         for (AccionApp a : vectorDeAcciones) {
             listAcciones.addItem(a);
             accionesIf.addItem(a);
             accionesElse.addItem(a);
+            accionesAAgregar.addItem(a);
         }
     }
 
@@ -54,52 +65,51 @@ public class SpheroMili {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String valores = "Rodar: " + angulo.getText() + ", " + rapidez.getText() + ", " + duracion.getText() + ".";
-                JOptionPane.showMessageDialog(null, valores);
                 int temp = Integer.parseInt(angulo.getText());
                 int temp2 = Integer.parseInt(rapidez.getText());
                 int temp3 = Integer.parseInt(duracion.getText());
-                vectorDeAcciones.add(new RodarAccionApp(temp, temp2, temp3));
-                ActualizarComboBox();
-
+                if ((temp == 0 | temp == 90 | temp == 180 | temp == 270) && (temp2 >= temp3) && (temp3>0)) {
+                    vectorDeAcciones.add(new RodarAccionApp(temp, temp2, temp3));
+                    ActualizarComboBox();
+                    //JOptionPane.showMessageDialog(null, valores);
+                } else {
+                    JOptionPane.showMessageDialog(null, " El ángulo tiene que ser 0, 90, 180, 270.\n La rapidez siempre tiene que ser mayor que la duración. \n La duración no puede ser 0. ");
+                }
             }
         });
         crearLuzButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String value = "Luz: " + r.getText() + ", " + g.getText() + ", " + b.getText() + ".";
-                JOptionPane.showMessageDialog(null, value);
                 int temp = Integer.parseInt(r.getText());
                 int temp2 = Integer.parseInt(g.getText());
                 int temp3 = Integer.parseInt(b.getText());
-                vectorDeAcciones.add(new LuzApp(temp, temp2, temp3));
-                ActualizarComboBox();
-            }
-        });
-        crearCondicionalButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String value = "If (" + atributo.getSelectedItem().toString() + " " + comparacion.getSelectedItem().toString() + " " + valor.getText() + ") {" + listAcciones.getSelectedItem().toString() + "}";
-                JOptionPane.showMessageDialog(null, value);
-                String temp = atributo.getSelectedItem().toString();
-                String temp2 = comparacion.getSelectedItem().toString();
-                int temp3 = Integer.parseInt(valor.getText());
-                AccionApp temp4 = (AccionApp) listAcciones.getSelectedItem();
-                vectorDeAcciones.add(new CondicionalApp(temp, temp2, temp3, temp4));
-                ActualizarComboBox();
+                if ((temp == 0 | temp == 255) && (temp2 == 0 | temp2 == 255) && (temp3 == 0 | temp3 == 255)){
+                    vectorDeAcciones.add(new LuzApp(temp, temp2, temp3));
+                    ActualizarComboBox();
+                    //JOptionPane.showMessageDialog(null, value);
+                }else {
+                    JOptionPane.showMessageDialog(null, "Valores válidos: 0 o 255");
+                }
+
             }
         });
         crearCondicionalElseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String value = "If (" + atributoelse.getSelectedItem().toString() + " " + comparador2.getSelectedItem().toString() + " " + valor2.getText() + ") {" + accionesIf.getSelectedItem().toString() + "}else {" + accionesElse.getSelectedItem().toString() + " }";
-                JOptionPane.showMessageDialog(null, value);
                 String temp = atributoelse.getSelectedItem().toString();
                 String temp2 = comparador2.getSelectedItem().toString();
                 int temp3 = Integer.parseInt(valor2.getText());
                 AccionApp temp4 = (AccionApp) accionesIf.getSelectedItem();
                 AccionApp temp5 = (AccionApp) accionesElse.getSelectedItem();
-                vectorDeAcciones.add(new CondicionalConElseApp(temp, temp2, temp3, temp4, temp5));
-                ActualizarComboBox();
+                if ((temp3 == 0 | temp3 == 90 | temp3 == 180 | temp3 == 270) ) {
+                    vectorDeAcciones.add(new CondicionalConElseApp(temp, temp2, temp3, temp4, temp5));
+                    ActualizarComboBox();
+                    //JOptionPane.showMessageDialog(null, value);
+                }else{
+                    JOptionPane.showMessageDialog(null, "El valor tiene que ser 0, 90, 180, 270.");
+                }
             }
         });
 
@@ -109,21 +119,54 @@ public class SpheroMili {
                 tablero.setText("");
                 Tablero t = new Tablero();
                 Pelota p = new Pelota();
-                DisplayPorPantalla d = new DisplayPorPantalla(t,tablero);
+                DisplayPorPantalla d = new DisplayPorPantalla(t, tablero);
                 Controlador c = new Controlador(t, p, d);
                 c.posicionarPelota(1, 1);
                 AccionCompuestaApp comp = new AccionCompuestaApp();
-                for (AccionApp ac : vectorDeAcciones) {
-                    comp.agregarAcciones(ac);
+                for (Object o : modelo.toArray()) {
+                    comp.agregarAcciones((AccionApp) o);
                 }
-                JOptionPane.showMessageDialog(null, c.ejecutarAccion(comp.convertirEnAcciones()));
+                c.ejecutarAccion(comp.convertirEnAcciones());
+                //JOptionPane.showMessageDialog(null, c.ejecutarAccion(comp.convertirEnAcciones()));
+            }
+        });
+        agregar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AccionApp temp = (AccionApp) accionesAAgregar.getSelectedItem();
+                //JOptionPane.showMessageDialog(null, "Accion agregada exitosamente");
+                modelo.addElement(temp);
+                list1.setModel(modelo);
+            }
+        });
+        borrarAccion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                modelo.remove(list1.getSelectedIndex());
+                list1.setModel(modelo);
+                //JOptionPane.showMessageDialog(null, "Accion borrada exitosamente");
+            }
+        });
+
+
+        codigoParaSpheroButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AccionCompuestaApp comp = new AccionCompuestaApp();
+                for (Object o : modelo.toArray()) {
+                    comp.agregarAcciones((AccionApp) o);
+                }
+                codigoToSphero.setText(createSpheroProgram(comp));
             }
         });
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Sphero");
-        frame.setContentPane(new SpheroMili().panel1);
+        frame.setSize(1024,768);
+        SpheroMili mili = new SpheroMili();
+        mili.panel1.setSize(1024,768);
+        frame.setContentPane(mili.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -131,6 +174,14 @@ public class SpheroMili {
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+
         listAcciones = new JComboBox<AccionApp>();
+        modelo = new DefaultListModel();
+
+    }
+
+    public String createSpheroProgram(AccionApp a) {
+        String temp = ("async function startProgram(){") + (a.toSpheroApp()) + ("}");
+        return temp;
     }
 }
